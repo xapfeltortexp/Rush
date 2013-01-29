ee1ee373d5d3c2bf749b4c7ec7706a81f3f350ca
xapfeltortexp <jan.hoeck@gmx.de>
2013-01-29 14:09:16 +0100

    Game

---
+++ src/com/LostForce/Object/Game/Game.java
@@ -0,0 +1,18 @@
+package com.LostForce.Object.Game;
+
+public class Game {
+	
+	String name = null;
+	
+	public Game(String name) {
+		setName(name);
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+}
--- src/com/LostForce/Object/Game/GameManager.java
+++ src/com/LostForce/Object/Game/GameManager.java
@@ -1,5 +1,15 @@
 package com.LostForce.Object.Game;
 
 public class GameManager {
+	
+	Game game = null;
+	
+	public Game getCurrentGame() {
+		return game;
+	}
+	
+	public void startGame(Game game) {
+		this.game = game;
+	}
 
 }
