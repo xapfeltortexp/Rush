2ab4c07efdf2987908e4002f1332fae46fea7f95
xapfeltortexp <jan.hoeck@gmx.de>
2013-01-29 14:39:29 +0100

    Added GameManager

--- src/com/LostForce/Commands/Commands.java
+++ src/com/LostForce/Commands/Commands.java
@@ -14,6 +14,7 @@ public class Commands implements CommandExecutor {
 	public Commands(Rush Rush) {
 		plugin = Rush;
 	}
+
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (!(sender instanceof Player)) {
@@ -21,21 +22,73 @@ public class Commands implements CommandExecutor {
 			return true;
 		}
 		Player player = (Player) sender;
-		if(cmd.getName().equalsIgnoreCase("rush")){
-			if(args.length == 0){
+		if (cmd.getName().equalsIgnoreCase("rush")) {
+			if (args.length == 0) {
 				player.sendMessage(ChatColor.RED + "Invalid usage!");
 				return false;
 			}
-			if(args[0].equalsIgnoreCase("create")){
-				if(args.length != 2){
+			if (args[0].equalsIgnoreCase("create")) {
+				if (args.length != 2) {
 					player.sendMessage(ChatColor.RED + "Invalid usage!");
 					player.sendMessage(ChatColor.GOLD + "/rush create <name>");
 					return false;
 				}
-				if(plugin.util.arenaExists(args[1])){
+				if (!player.hasPermission("rush.arena.create")) {
+					player.sendMessage(ChatColor.RED + "You dont have Permissions!");
+					return true;
+				}
+				if (plugin.util.arenaExists(args[1])) {
 					player.sendMessage(plugin.prefix + ChatColor.RED + "An arena with that name already exists!");
 					return true;
 				}
+			} else if (args[0].equalsIgnoreCase("delete")) {
+				if (args.length != 2) {
+					player.sendMessage(ChatColor.RED + "Invalid usage!");
+					player.sendMessage(ChatColor.GOLD + "/rush delete <name>");
+					return false;
+				}
+				if (!player.hasPermission("rush.arena.delete")) {
+					player.sendMessage(ChatColor.RED + "You dont have Permissions!");
+					return true;
+				}
+				if (!plugin.util.arenaExists(args[1])) {
+					player.sendMessage(plugin.prefix + ChatColor.RED + "An arena with that does not exists!");
+					return true;
+				}
+			} else if (args[0].equalsIgnoreCase("arenas")) {
+
+			} else if (args[0].equalsIgnoreCase("kick")) {
+				if (args.length != 2) {
+					player.sendMessage(ChatColor.RED + "Invalid usage!");
+					player.sendMessage(ChatColor.GOLD + "/rush kick <player>");
+					return false;
+				}
+				if (!player.hasPermission("rush.kick")) {
+					player.sendMessage(ChatColor.RED + "You dont have Permissions!");
+					return true;
+				}
+			} else if (args[0].equalsIgnoreCase("stop")) {
+				if (args.length != 2) {
+					player.sendMessage(ChatColor.RED + "Invalid usage!");
+					player.sendMessage(ChatColor.GOLD + "/rush kick <player>");
+					return false;
+				}
+				if (!player.hasPermission("rush.kick")) {
+					player.sendMessage(ChatColor.RED + "You dont have Permissions!");
+					return true;
+				}
+			} else if (args[0].equalsIgnoreCase("start")) {
+				if (args.length != 2) {
+					player.sendMessage(ChatColor.RED + "Invalid usage!");
+					player.sendMessage(ChatColor.GOLD + "/rush kick <player>");
+					return false;
+				}
+				if (!player.hasPermission("rush.kick")) {
+					player.sendMessage(ChatColor.RED + "You dont have Permissions!");
+					return true;
+				}
+			} else if(args[0].equalsIgnoreCase("join")) {
+				
 			}
 		}
 		return false;
--- src/com/LostForce/Object/Game/GameManager.java
+++ src/com/LostForce/Object/Game/GameManager.java
@@ -1,8 +1,15 @@
 package com.LostForce.Object.Game;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.LostForce.Object.RushPlayer;
+
 public class GameManager {
 	
 	Game game = null;
+	List<RushPlayer> players = new LinkedList<RushPlayer>();
 	
 	public Game getCurrentGame() {
 		return game;
@@ -10,6 +17,10 @@ public class GameManager {
 	
 	public void startGame(Game game) {
 		this.game = game;
+	}
+	
+	public List<RushPlayer> getPlayers() {
+		return players;
 	}
 
 }
